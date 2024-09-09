1648693426b66cadcd4195210fefbe1c3a21df8f
LemonNekoGH <self@lemonneko.moe>
2024-09-09 17:40:34 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Application } from 'vue3-pixi'
+
+import { settings } from 'pixi.js'
+import MinimalReproduction from './MinimalReproduction.vue'
+import { ref } from 'vue'
+
+settings.RESOLUTION = window.devicePixelRatio
+
+const components = ref([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }])
+</script>
+
+<template>
+  <div class="container">
+    <application :width="480" :height="270">
+      <MinimalReproduction v-for="c in components" :key="c.id" :x="(c.id - 1) * 100" />
+    </application>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
---
+++ src/MinimalReproduction.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import texture from '@/assets/texture.png'
+import { ref } from 'vue'
+
+const exist = ref(true)
+</script>
+
+<template>
+  <container>
+    <sprite :texture="texture" v-if="exist"></sprite>
+    <text v-if="exist" :y="50" :style="{ fill: 'white' }">
+      Hello
+    </text>
+    <text :y="100" :style="{ fill: 'white' }" @click="exist = !exist">
+      {{ exist ? 'remove' : 'add' }}
+    </text>
+  </container>
+</template>
